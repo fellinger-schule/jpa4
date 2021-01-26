1c3318bd5e106a16e37227b4872cbfb3478d765b
chris <christanangelov@gmail.com>
2021-01-26 19:17:01 +0100

    CustomerService

--- src/main/java/at/htl/workloads/customer/CustomerService.java
+++ src/main/java/at/htl/workloads/customer/CustomerService.java
@@ -1,4 +1,12 @@
 package at.htl.workloads.customer;
 
+import at.htl.model.kino.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    Customer getCustomerById(long id);
+    boolean addCustomer(CustomerDTO customer);
+    boolean removeCustomer(CustomerDTO customer);
+    List<Customer> getAllCustomers();
 }
--- src/main/java/at/htl/workloads/customer/CustomerServiceImpl.java
+++ src/main/java/at/htl/workloads/customer/CustomerServiceImpl.java
@@ -1,6 +1,10 @@
 package at.htl.workloads.customer;
 
+import at.htl.model.kino.CustomerDTO;
+import at.htl.model.person.PersonDTO;
 import at.htl.workloads.person.PersonRepository;
+
+import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService{
     public final CustomerRepository CustomerRepository;
@@ -9,4 +13,27 @@ public class CustomerServiceImpl implements CustomerService{
         this.CustomerRepository = cr;
     }
 
+    @Override
+    public Customer getCustomerById(long id) {
+        return null;
+    }
+
+    @Override
+    public boolean addCustomer(CustomerDTO customer) {
+        var exists = CustomerRepository.getCustomerById((customer.getId()));
+        if (exists != null) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeCustomer(CustomerDTO customer) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
 }
