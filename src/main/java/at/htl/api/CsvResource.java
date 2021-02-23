82e7d873db7c7007b90816f7a537bb8c80519cf3
Amel-Dzogovic <tOKYO-2020>
2021-02-23 20:00:51 +0100

    aktuell

--- src/main/java/at/htl/api/CsvResource.java
+++ src/main/java/at/htl/api/CsvResource.java
@@ -1,28 +1,50 @@
 package at.htl.api;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
+import at.htl.model.kino.PersonDTO;
+import at.htl.workloads.person.PersonService;
+
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 
 
 @Path("/api/Csv")
 public class CsvResource {
 
-    @Inject
-    private EntityManager em ;
 
-    @POST
+
+
+    private final PersonService personService;
+
+    public CsvResource(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GET
     @Path("init")
     @Produces(MediaType.APPLICATION_JSON)
     public String fillData(){
+        String line = "";
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("Customer_Data.csv"));
+
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+                String[] Values = line.split(",");
+                PersonDTO newp = new PersonDTO(Long.parseLong(Values[0]),Values[1]);
+                personService.addPerson(newp);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "ok";
     }
 }
